ae159f878f97a8cc2829496d8471c79fe3327c7b
ollydevulder <ollydevulder@gmail.com>
2020-10-29 00:01:17 +0000

    Fix urgent staticfile bug

--- thingreview/settings/common.py
+++ thingreview/settings/common.py
@@ -26,6 +26,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'thingreview.urls'
@@ -91,3 +92,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
