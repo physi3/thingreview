15d98758a928fd11ae8e02a087194e532d90b192
ollydevulder <ollydevulder@gmail.com>
2020-10-31 00:57:20 +0000

    Add tests for #6, #7

--- things/tests/test_views.py
+++ things/tests/test_views.py
@@ -9,3 +9,16 @@ class RandomViewTests(TestCase):
         res = client.get(reverse('things:random'))
         self.assertEqual(res.status_code, 302)
 
+
+class LatestViewTests(TestCase):
+    def test_redirect_on_empty_thing_table(self):
+        client = Client()
+        res = client.get(reverse('things:latest'))
+        self.assertEqual(res.status_code, 302)
+
+
+class LeastReviewedViewTest(TestCase):
+    def test_redirect_on_empty_thing_table(self):
+        client = Client()
+        res = client.get(reverse('things:least_reviewed'))
+        self.assertEqual(res.status_code, 302)
